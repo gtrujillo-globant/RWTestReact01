b8e0cecc2d89aee617a360a0a63ec6b8f538dc82
Gabriel Trujillo C <joaqt23@gmail.com>
2018-01-21 17:02:40 -0500

    Simple component

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello, parcel!!!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MyComponent = () => (
+    <div>Hello, React</div>
+);
+
+const mountNode = document.getElementById('app');
+ReactDOM.render(<MyComponent />, mountNode);

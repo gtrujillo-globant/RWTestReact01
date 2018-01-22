97b974a7a95c992b3716eb11801fc6da8c445e10
Gabriel Trujillo C <joaqt23@gmail.com>
2018-01-22 00:20:24 -0500

    Basic counter

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const MyComponent = () => (
-    <div>Hello, React</div>
-);
+class MyCounter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 };
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler() {
+        this.setState({ counter: this.state.counter + 1 });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>setState piftalls</h1>
+                <div>Counter is: {this.state.counter}</div>
+                <button onClick={this.clickHandler}>1Up!</button>
+            </div>
+        )
+    }
+}
 
 const mountNode = document.getElementById('app');
-ReactDOM.render(<MyComponent />, mountNode);
+ReactDOM.render(<MyCounter />, mountNode);
